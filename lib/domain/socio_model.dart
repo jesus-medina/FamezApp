0d4376730319f3eaa06f4c93492427df00a0a966
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2020-05-24 11:30:53 -0700

    Contacto page launch url for each socio

--- lib/domain/socio_model.dart
+++ lib/domain/socio_model.dart
@@ -1,36 +1,44 @@
+import 'package:flutter/material.dart';
+
 class Socio {
   String firstName;
   String lastName;
+  String imagePath;
   String phoneNumber;
   String email;
   String urlFacebook;
   String urlWhatsApp;
 
   Socio({
-    this.firstName,
-    this.lastName,
-    this.phoneNumber,
-    this.email,
-    this.urlFacebook,
-    this.urlWhatsApp
+    @required this.firstName,
+    @required this.lastName,
+    @required this.imagePath,
+    @required this.phoneNumber,
+    @required this.email,
+    @required this.urlFacebook,
+    @required this.urlWhatsApp
   });
+
+  String fullName() => '$firstName $lastName';
 }
 
 List<Socio> getSocios() => [
   Socio(
-    firstName: 'Jesús Daniel',
-    lastName: 'Medina Cruz',
-    phoneNumber: '6631036365',
-    email: 'jesus.daniel.medina.cruz@gmail.com',
-    urlFacebook: 'https://www.facebook.com/jesusdaniel.medinacruz',
-    urlWhatsApp: 'https://wa.me/526631036365'
-  ),
-  Socio(
     firstName: 'Danna Priscila',
     lastName: 'Hernández García',
-    phoneNumber: '6645770090',
+    imagePath: 'images/danna.jpg',
+    phoneNumber: '+52-664-577-0090',
     email: 'hernandezgdpr@gmail.com',
     urlFacebook: 'https://www.facebook.com/dannaa.garciia',
     urlWhatsApp: 'https://wa.me/526645770090'
+  ),
+  Socio(
+    firstName: 'Jesús Daniel',
+    lastName: 'Medina Cruz',
+    imagePath: 'images/daniel.jpg',
+    phoneNumber: '+52-663-103-6365',
+    email: 'jesus.daniel.medina.cruz@gmail.com',
+    urlFacebook: 'https://www.facebook.com/jesusdaniel.medinacruz',
+    urlWhatsApp: 'https://wa.me/526631036365'
   ),
 ];
--- lib/pages/contacto_page.dart
+++ lib/pages/contacto_page.dart
@@ -1,6 +1,11 @@
-import 'package:famezapp/utils/platform_scaffold.dart';
+import 'package:famezapp/domain/familia_model.dart';
+import 'package:famezapp/domain/socio_model.dart';
+import 'package:famezapp/utils/colors.dart';
+import 'package:famezapp/utils/font_awesome_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ContactoPage extends StatefulWidget {
   @override
@@ -10,16 +15,140 @@ class ContactoPage extends StatefulWidget {
 
 class _ContactoPageState extends State<ContactoPage> {
 
+  Familia familia;
+
   @override
   Widget build(BuildContext context) {
     _initContactoInfoFromFirestore();
     return Scaffold(
-      body: Center(child: Text('We\'re inside Contacto page'))
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(32),
+                child: Text('Familia',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold
+                ))
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(familia.lastName,
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold
+                ))
+            ]
+          ),
+          Divider(),
+          Column(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.home, color: FamezColors.primaryDarkColor),
+                title: Text(familia.address),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FlatButton.icon(
+                    icon: Icon(Icons.map, color: FamezColors.primaryDarkColor),
+                    label: Text('VER EN MAPA', style: TextStyle(color: FamezColors.primaryDarkColor)),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+              Divider(),
+              _contactoTile(familia.members[0]),
+              Divider(),
+              _contactoTile(familia.members[1]),
+              Divider(),
+            ],
+          ),
+        ]
+      )
     );
   }
 
   void _initContactoInfoFromFirestore() {
-    
+    setState(() {
+      familia = getFamilia();
+    });
   }
 
+  Widget _contactoTile(Socio socio) => Column(
+    children: [
+      ListTile(
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(32),
+          child: Image.asset(
+            socio.imagePath,
+            width: 48,
+            height: 48,
+            fit: BoxFit.none,
+            scale: 12,
+            alignment: Alignment(0.0, -0.65),
+          ),
+        ),
+        title: Text(socio.fullName()),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _contactoIconButton(
+            icon: Icons.phone,
+            onPressed: () {
+              _launchURL('tel:${socio.phoneNumber}');
+            },
+          ),
+          _contactoIconButton(
+            icon: Icons.message,
+            onPressed: () {
+              _launchURL('sms:${socio.phoneNumber}');
+            },
+          ),
+          _contactoIconButton(
+            icon: Icons.email,
+            onPressed: () {
+              _launchURL('mailto:${socio.email}');
+            },
+          ),
+          _contactoIconButton(
+            icon: FontAwesome.facebook,
+            onPressed: () {
+              _launchURL(socio.urlFacebook);
+            },
+          ),
+          _contactoIconButton(
+            icon: FontAwesome.whatsapp,
+            onPressed: () {
+              _launchURL(socio.urlWhatsApp);
+            },
+          ),
+        ],
+      )
+    ],
+  );
+
+  IconButton _contactoIconButton({IconData icon, void Function() onPressed}) =>
+    IconButton(
+      icon: Icon(icon), color: FamezColors.secondaryDarkColor,
+      onPressed: onPressed,
+    );
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      Fluttertoast.showToast(
+        msg: 'Esta acción no está disponible',
+        toastLength: Toast.LENGTH_LONG
+      );
+    }
+  }
 }
